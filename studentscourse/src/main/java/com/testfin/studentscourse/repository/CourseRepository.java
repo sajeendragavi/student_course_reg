747aa614c36cd2c9bf4e06c3b03055fb9b433ad5
Gavindu sajeendra De Silva <43973552+sajeendragavi@users.noreply.github.com>
2025-06-07 00:33:14 +0530

    Delete studentscourse directory

--- studentscourse/src/main/java/com/testfin/studentscourse/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testfin.studentscourse.repository;
-
-import com.testfin.studentscourse.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course,Long> {
-}
--- studentscourse/src/main/java/com/testfin/studentscourse/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testfin.studentscourse.repository;
-
-import com.testfin.studentscourse.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
